83070f6643a31c0b24249ec0fe5bad730997da15
Thet Minn Htun <thetminnhtun92@gmail.com>
2020-04-11 22:24:45 +0630

    Laravel 7 File Upload - File upload to storage directory

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class HomeController extends Controller
 {
@@ -14,7 +15,8 @@ class HomeController extends Controller
     public function store(Request $request)
     {
         $filename = time() . '_' . $request->file('image')->getClientOriginalName();
-        $request->file('image')->move(public_path('image'), $filename);
+        // $request->file('image')->move(public_path('image'), $filename);
+        $request->file('image')->storeAs('upload', $filename);
         return back();
     }
 }
